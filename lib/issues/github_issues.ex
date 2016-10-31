b28d45ca675f8713781e1e29f893755336c8f204
Scott Meade <scott@synapsoftware.com>
2016-10-31 07:12:48 -0600

    Convert Response

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -7,7 +7,8 @@
 # Visit http://www.pragmaticprogrammer.com/titles/elixir13 for more book information.
 #---
 defmodule Issues.GithubIssues do
-  @user_agent  [ {"User-agent", "Elixir scott@synapsoftware.com"} ]
+  @user_agent [ {"User-agent", "Elixir scott@synapsoftware.com"} ]
+  @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
     issues_url(user, project)
@@ -16,14 +17,14 @@ defmodule Issues.GithubIssues do
   end
 
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   def handle_response({ :ok, %{status_code: 200, body: body}}) do
-    { :ok,    body }
+    { :ok,    Poison.Parser.parse!(body) }
   end
 
   def handle_response({ _,   %{status_code: _,   body: body}}) do
-    { :error, body }
+    { :error, Poison.Parser.parse!(body) }
   end
 end
